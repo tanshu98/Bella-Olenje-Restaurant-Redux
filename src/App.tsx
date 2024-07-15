1e8b70af719a77886669f87c8d31d1731d970093
Tanshu <tanshuallewar98@gmail.com>
2024-07-15 18:41:26 +0530

    Added react router dom with dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import LandingPage from './components/Landing Page/LandingPage';
+import LoginSingUp from './components/Login SignUp/LoginSignUp';
+import Navbar from './components/Navbar/Navbar';
+import Product from './components/Product/Product';
+import Cart from './components/Cart/Cart';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+
+   {/* <Navbar /> */}
+   <Routes>
+    <Route path='/' element={<LandingPage />} />
+    <Route path='/login-signup' element={<LoginSingUp />} />
+    <Route path='/home' element={<Home />} />
+    <Route path='/Product' element={<Product />} />
+    <Route path='/cart' element={<Cart />} />
+    <Route path='*' element={<PageNotFound />} />
+   </Routes>
+   </BrowserRouter>
   );
 }
 
